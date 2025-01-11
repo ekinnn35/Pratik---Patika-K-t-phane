140201305c6d27df27b84ec4ed0b0fa2bab600b2
ekinnn35 <ekinn.9193@gmail.com>
2025-01-11 16:41:06 +0300

    Add project files.

---
+++ Pratik - Patika Kütüphane/Kitap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KutuphaneUygulamasi
+{
+    public class Kitap
+    {
+        // Kitap özellikleri
+        public string Ad { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayınevi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+        // Default Constructor
+        public Kitap()
+        {
+            Console.WriteLine("Bir kitap kaydedildi.");
+            KayitTarihi = DateTime.Now; // Kayıt tarihi şu an olarak atanır
+        }
+
+        // Parametreli Constructor
+        public Kitap(string ad, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinevi)
+        {
+            Console.WriteLine("Bir kitap kaydedildi.");
+            Ad = ad;
+            YazarAdi = yazarAdi;
+            YazarSoyadi = yazarSoyadi;
+            SayfaSayisi = sayfaSayisi;
+            Yayınevi = yayinevi;
+            KayitTarihi = DateTime.Now; // Kayıt tarihi şu an olarak atanır
+        }
+
+        // Bilgileri yazdıran metot
+        public void BilgiYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Yazar: {YazarAdi} {YazarSoyadi}, Sayfa Sayısı: {SayfaSayisi}, Yayınevi: {Yayınevi}, Kayıt Tarihi: {KayitTarihi.ToShortDateString()}");
+        }
+    }
+}
---
+++ Pratik - Patika Kütüphane/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using KutuphaneUygulamasi;
+
+namespace KutuphaneUygulamasiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Default constructor ile nesne oluşturma
+            Kitap kitap1 = new Kitap
+            {
+                Ad = "Bir Kitap",
+                YazarAdi = "Ahmet",
+                YazarSoyadi = "Yılmaz",
+                SayfaSayisi = 320,
+                Yayınevi = "Alfa Yayınları"
+            };
+
+            // Parametreli constructor ile nesne oluşturma
+            Kitap kitap2 = new Kitap("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayıncılık");
+
+            // Diğer kitaplar
+            Kitap kitap3 = new Kitap("Daha Başka Kitap", "Mehmet", "Demir", 450, "Gamma Yayınları");
+            Kitap kitap4 = new Kitap("Son Kitap", "Elif", "Şahin", 275, "Delta Yayınları");
+            Kitap kitap5 = new Kitap("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
+
+            // Kitap bilgilerini yazdırma
+            kitap1.BilgiYazdir();
+            kitap2.BilgiYazdir();
+            kitap3.BilgiYazdir();
+            kitap4.BilgiYazdir();
+            kitap5.BilgiYazdir();
+
+            Console.ReadKey(); // Konsolu açık tutar
+        }
+    }
+}
